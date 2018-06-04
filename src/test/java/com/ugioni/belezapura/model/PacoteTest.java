48d3e704fa80651de4def9d1dabfcc28411283c6
ugioni <leandro_ugioni@hotmail.com>
2018-06-04 20:44:58 -0300

    add unit tests

--- src/test/java/com/ugioni/belezapura/model/PacoteTest.java
+++ src/test/java/com/ugioni/belezapura/model/PacoteTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PacoteTest {
 
@@ -13,7 +13,7 @@ public class PacoteTest {
         Pacote pacote = new Pacote();
         pacote.setId(1L);
         pacote.setDescricao("pacote 01");
-        pacote.setEspecificacao("test");
+        pacote.setEspecificacao("especificação 01");
         pacote.setSecoes(1);
         pacote.setValorSecao(BigDecimal.TEN);
         pacote.setValorTotal(BigDecimal.TEN);
@@ -21,8 +21,44 @@ public class PacoteTest {
     }
 
     @Test
+    @DisplayName("Testando getId")
+    public void testGetId() {
+        assertTrue(pacote().getId() == 1L);
+    }
+
+    @Test
     @DisplayName("Testando getDescricao")
     public void testGetDescricao() {
         assertEquals(pacote().getDescricao(), "pacote 01");
+    }
+
+    @Test
+    @DisplayName("Testando getEspecificacao")
+    public void testGetEspecificacao() {
+        assertEquals(pacote().getEspecificacao(), "especificação 01");
+    }
+
+    @Test
+    @DisplayName("Testando getSecoes")
+    public void testGetSecoes() {
+        assertTrue(pacote().getSecoes() == 1);
+    }
+
+    @Test
+    @DisplayName("Testando getValorSecao")
+    public void testGetValorSecao() {
+        assertEquals(pacote().getValorSecao(), BigDecimal.TEN);
+    }
+
+    @Test
+    @DisplayName("Testando getValorTotal")
+    public void testGetValorTotal() {
+        assertEquals(pacote().getValorTotal(), BigDecimal.TEN);
+    }
+
+    @Test
+    @DisplayName("Testando toString")
+    public void testToString() {
+        assertNotNull(pacote().toString());
     }
 }
