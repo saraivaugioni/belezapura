0099bd6f633b3a64c429073fe0a6eb0db03a5154
ugioni <leandro_ugioni@hotmail.com>
2018-06-04 22:28:22 -0300

    add unit tests

---
+++ src/test/java/com/ugioni/belezapura/model/ClienteTest.java
@@ -0,0 +1,48 @@
+package com.ugioni.belezapura.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ClienteTest {
+
+    private Cliente cliente() {
+        Cliente cliente = new Cliente();
+        cliente.setId(1L);
+        cliente.setNome("Leandro Ugioni");
+        cliente.setTelefone("48 9919 5252");
+        cliente.setEmail("a@b.com.br");
+        return cliente;
+    }
+
+    @Test
+    @DisplayName("Testando getId")
+    public void testGetId() {
+        assertTrue(cliente().getId() == 1L);
+    }
+
+    @Test
+    @DisplayName("Testando getNome")
+    public void testGetNome() {
+        assertEquals(cliente().getNome(), "Leandro Ugioni");
+    }
+
+    @Test
+    @DisplayName("Testando getTelefone")
+    public void testGetTelefone() {
+        assertEquals(cliente().getTelefone(), "48 9919 5252");
+    }
+
+    @Test
+    @DisplayName("Testando getEmail")
+    public void testGetEmail() {
+        assertEquals(cliente().getEmail(), "a@b.com.br");
+    }
+
+    @Test
+    @DisplayName("Testando toString")
+    public void testToString() {
+        assertNotNull(cliente().toString());
+    }
+}
---
+++ src/test/java/com/ugioni/belezapura/model/EquipamentoTest.java
@@ -0,0 +1,58 @@
+package com.ugioni.belezapura.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class EquipamentoTest {
+
+    private Equipamento equipamento() {
+        Equipamento equipamento = new Equipamento();
+        equipamento.setId(1L);
+        equipamento.setDescricao("Heccus Turbo");
+        equipamento.setEspecificacao("especificação 01");
+        equipamento.setPreco(BigDecimal.TEN);
+        equipamento.setDataAquisicao(new Date(2018, 1, 1));
+        return equipamento;
+    }
+
+    @Test
+    @DisplayName("Testando getId")
+    public void testGetId() {
+        assertTrue(equipamento().getId() == 1L);
+    }
+
+    @Test
+    @DisplayName("Testando getDescricao")
+    public void testGetDescricao() {
+        assertEquals(equipamento().getDescricao(), "Heccus Turbo");
+    }
+
+    @Test
+    @DisplayName("Testando getEspecificacao")
+    public void testGetEspecificacao() {
+        assertEquals(equipamento().getEspecificacao(), "especificação 01");
+    }
+
+    @Test
+    @DisplayName("Testando getPreco")
+    public void testGetPreco() {
+        assertEquals(equipamento().getPreco(), BigDecimal.TEN);
+    }
+
+    @Test
+    @DisplayName("Testando getDataAquisicao")
+    public void testGetDataAquisicao() {
+        assertEquals(equipamento().getDataAquisicao(), new Date(2018, 1, 1));
+    }
+
+    @Test
+    @DisplayName("Testando toString")
+    public void testToString() {
+        assertNotNull(equipamento().toString());
+    }
+}
