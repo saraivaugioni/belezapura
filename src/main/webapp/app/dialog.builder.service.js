af9abe50ffcf8ed71931ef10e2ca9ac48a42705e
ugioni <leandro_ugioni@hotmail.com>
2018-06-19 19:18:09 -0300

    fix config dialog builder

--- src/main/webapp/app/dialog.builder.service.js
+++ src/main/webapp/app/dialog.builder.service.js
@@ -7,14 +7,14 @@
             confirm: swal.mixin({
                 type: 'question',
                 showCancelButton: true,
-                confirmButtonText: 'Sim, claro!',
-                cancelButtonText: 'Não, deixa',
-                confirmButtonClass: 'btn btn-success mx-2',
-                cancelButtonClass: 'btn btn-danger mx-2',
+                confirmButtonText: 'Sim',
+                cancelButtonText: 'Não',
+                confirmButtonClass: 'btn btn-success btn-lg mx-2',
+                cancelButtonClass: 'btn btn-danger btn-lg mx-2',
                 buttonsStyling: false
             }),
             message: swal.mixin({
-                position: 'top-end',
+                position: 'center',
                 type: 'success',
                 showConfirmButton: false,
                 timer: 1500
